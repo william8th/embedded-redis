398966e1af23af83020ad80ad3edcf66306ca3b5
William Heng <william.heng@bjss.com>
2016-09-15 12:19:56 +0100

    Try to create temporary directory in the current working directory

--- src/main/java/redis/embedded/util/JarUtil.java
+++ src/main/java/redis/embedded/util/JarUtil.java
@@ -1,16 +1,17 @@
 package redis.embedded.util;
 
-import com.google.common.io.Files;
 import com.google.common.io.Resources;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class JarUtil {
 
     public static File extractExecutableFromJar(String executable) throws IOException {
-        File tmpDir = Files.createTempDir();
+        File tmpDir = Files.createDirectory(Paths.get("temp")).toFile();
         tmpDir.deleteOnExit();
 
         File command = new File(tmpDir, executable);
